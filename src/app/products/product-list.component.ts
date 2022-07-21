1ff1e8f1c6ad720a19e0982854f1880c5ee564ed
Alex Lamb <alexlamb@Alexs-MacBook-Pro.local>
2022-07-21 14:27:16 -0600

    Hide Show Image button working

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -6,7 +6,10 @@ import { Component } from "@angular/core";
 })
 
 export class ProductListComponent{
-    pageTitle: string = "Product List";
+    pageTitle = "Product List";
+    imageWidth: number = 50;
+    imageMargin: number = 2;
+    showImage: boolean = false;
     products: any[] = [{
     "productId": 1,
     "productName": "Leaf Rake",
@@ -27,4 +30,8 @@ export class ProductListComponent{
     "starRating": 4.2,
     "imageUrl": "assets/images/garden_cart.png"
   },]
+
+  toggleImage(): void {
+    this.showImage = !this.showImage;
+  }
 }
