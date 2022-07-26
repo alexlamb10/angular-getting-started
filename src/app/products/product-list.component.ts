54fb5fbf1634899aa18eb97cae071ad4e254b5e8
Alex Lamb <alexlamb@Alexs-MacBook-Pro.local>
2022-07-26 17:05:00 -0600

    Filter works to filter through items

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -20,8 +20,10 @@ export class ProductListComponent implements OnInit{
   set listFilter(value: string) {
     this._listFilter = value;
     console.log('In setter:', value)
+    this.filteredProducts = this.performFilter(value)
   }
 
+  filteredProducts: IProduct[] = [];
     products: IProduct[] = [{
     "productId": 1,
     "productName": "Leaf Rake",
@@ -42,6 +44,11 @@ export class ProductListComponent implements OnInit{
     "starRating": 4.2,
     "imageUrl": "assets/images/garden_cart.png"
   },]
+
+  performFilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter((product: IProduct) => product.productName.toLocaleLowerCase().includes(filterBy))
+  }
 
   toggleImage(): void {
     this.showImage = !this.showImage;
