296ac39bfd157fc69fc8dd1de0d7b56932860a12
Alex Lamb <alexlamb@Alexs-MacBook-Pro.local>
2022-08-01 17:53:29 -0600

    Set up Observable and HttpClient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http'
 
 import { ProductListComponent } from './products/product-list.component';
 import { AppComponent } from './app.component';
@@ -13,7 +14,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   bootstrap: [AppComponent]
 })
