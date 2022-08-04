991f25928650e53b871879443be1a130328ea06e
Alex Lamb <alexlamb104@gmail.com>
2022-08-04 12:54:40 -0600

    Shared Module complete sharing StarComponent and ConvertToSpacesPipe

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -1,23 +1,16 @@
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-import { StarComponent } from './../shared/star.component';
-import { ConvertToSpacesPipe } from './../shared/convert-to-spaces.pipe';
 import { ProductDetailComponent } from './product-detail.component';
 import { ProductListComponent } from './product-list.component';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ProductDetailGuard } from './product-detail.guard';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
-    ConvertToSpacesPipe,
-    StarComponent
   ],
   imports: [
-    CommonModule,
-    FormsModule,
     RouterModule.forChild([
       {path: 'products', component: ProductListComponent},
       {
@@ -25,7 +18,8 @@ import { ProductDetailGuard } from './product-detail.guard';
             canActivate: [ProductDetailGuard],
             component: ProductDetailComponent
         },
-    ])
+    ]),
+    SharedModule
   ]
 })
 export class ProductModule { }
